ebdbf4a2f80892fecfe571063b1a49f91d90ad6f
chimps <chimpss@163.com>
2019-04-20 10:31:46 +0800

    study

--- test.sh
+++ test.sh
@@ -7,9 +7,9 @@ GOMAXPROCS=4 go test -timeout 90s -race $(go list ./... | grep -v /vendor/)
 # no tests, but a build is something
 for dir in apps/*/ bench/*/; do
     dir=${dir%/}
-    if grep -q '^package main$' $dir/*.go 2>/dev/null; then
+    if grep -q '^package main$' ${dir}/*.go 2>/dev/null; then
         echo "building $dir"
-        go build -o $dir/$(basename $dir) ./$dir
+        go build -o ${dir}/$(basename ${dir}) ./${dir}
     else
         echo "(skipped $dir)"
     fi
--- travis.sh
+++ travis.sh
@@ -3,7 +3,7 @@
 set -e
 
 go_minor_version=$(go version | awk '{print $3}' | awk -F. '{print $2}')
-if [[ $go_minor_version -gt 10 ]]; then
+if [[ ${go_minor_version} -gt 10 ]]; then
     export GO111MODULE=on
 else
     wget -O dep https://github.com/golang/dep/releases/download/v0.5.0/dep-linux-amd64
